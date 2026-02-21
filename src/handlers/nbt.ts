fbcf4770edd8e69de22610d73b7121d390b79c6d
Logan Larson <75538160+Larson-Logan@users.noreply.github.com>
2026-02-20 23:53:34 -0600

    feat: Implement a schematic converter for Litematica, Sponge, and Minecraft schematic formats with NBT handling.

--- src/handlers/nbt.ts
+++ src/handlers/nbt.ts
@@ -1,6 +1,7 @@
 import type { FileData, FileFormat, FormatHandler } from "src/FormatHandler";
 import * as NBT from "nbtify";
 import CommonFormats from "src/CommonFormats";
+import { gzipSync, gunzipSync } from "fflate";
 
 class nbtHandler implements FormatHandler {
     public name: string = "nbt";
@@ -22,6 +23,39 @@ class nbtHandler implements FormatHandler {
                 category: "data",
                 lossless: true
             },
+            {
+                name: "Minecraft Schematic",
+                format: "SCHEMATIC",
+                extension: "schematic",
+                mime: "application/x-minecraft-schematic",
+                from: true,
+                to: true,
+                internal: "schematic",
+                category: "data",
+                lossless: true
+            },
+            {
+                name: "Sponge Schematic",
+                format: "SCHEM",
+                extension: "schem",
+                mime: "application/x-minecraft-schem",
+                from: true,
+                to: true,
+                internal: "schem",
+                category: "data",
+                lossless: true
+            },
+            {
+                name: "Litematica Schematic",
+                format: "LITEMATIC",
+                extension: "litematic",
+                mime: "application/x-minecraft-litematic",
+                from: true,
+                to: true,
+                internal: "litematic",
+                category: "data",
+                lossless: true
+            },
             CommonFormats.JSON.supported("json", true, true, true),
             {
                 name: "String Named Binary Tag",
@@ -33,39 +67,6 @@ class nbtHandler implements FormatHandler {
                 internal: "snbt",
                 category: "data",
                 lossless: true // only compression data is lost
-            },
-            {
-                name: "Minecraft Schematic",
-                format: "SCHEMATIC",
-                extension: "schematic",
-                mime: "application/x-minecraft-schematic",
-                from: true,
-                to: true,
-                internal: "nbt",
-                category: "data",
-                lossless: true
-            },
-            {
-                name: "Sponge Schematic",
-                format: "SCHEM",
-                extension: "schem",
-                mime: "application/x-minecraft-schem",
-                from: true,
-                to: true,
-                internal: "nbt",
-                category: "data",
-                lossless: true
-            },
-            {
-                name: "Litematica Schematic",
-                format: "LITEMATIC",
-                extension: "litematic",
-                mime: "application/x-minecraft-litematic",
-                from: true,
-                to: true,
-                internal: "nbt",
-                category: "data",
-                lossless: true
             },
         ]
         this.ready = true
@@ -81,44 +82,74 @@ class nbtHandler implements FormatHandler {
         const decoder = new TextDecoder()
         const encoder = new TextEncoder()
 
-        // nbt <-> json
-        if (inputFormat.internal == "nbt" && outputFormat.internal == "json") {
+        // nbt / schem / schematic / litematic -> json
+        if ((inputFormat.internal == "nbt" || inputFormat.internal == "schem" || inputFormat.internal == "schematic" || inputFormat.internal == "litematic") && outputFormat.internal == "json") {
             for (const file of inputFiles) {
-                const nbt = await NBT.read(file.bytes);
-                const j = JSON.stringify(nbt.data, null, this.indent)
+                let unzipped = file.bytes;
+                try {
+                    // intermediate NBT from schematicConverter will NOT be zipped, but native schems will be
+                    if (inputFormat.internal == "schem" || inputFormat.internal == "schematic" || inputFormat.internal == "litematic") {
+                        unzipped = gunzipSync(file.bytes);
+                    }
+                } catch (e) {}
+
+                const nbt = await NBT.read(unzipped);
+                const j = JSON.stringify(nbt.data, (key, value) =>
+                    typeof value === 'bigint' ? value.toString() : value,
+                this.indent);
                 outputFiles.push({
                     name: file.name.split(".")[0] + ".json",
                     bytes: encoder.encode(j)
-                })
+                });
             }
         }
-        if (inputFormat.internal == "json" && outputFormat.internal == "nbt") {
+        // json -> nbt / schem / schematic / litematic
+        if (inputFormat.internal == "json" && (outputFormat.internal == "nbt" || outputFormat.internal == "schem" || outputFormat.internal == "schematic" || outputFormat.internal == "litematic")) {
             for (const file of inputFiles) {
                 const text = decoder.decode(file.bytes)
                 const obj = JSON.parse(text)
-                const bd = await NBT.write(obj)
+                let bd = await NBT.write(obj)
+                
+                if (outputFormat.internal == "schem" || outputFormat.internal == "schematic" || outputFormat.internal == "litematic") {
+                    // Schematics require gzipping
+                     bd = gzipSync(bd);
+                }
+
                 outputFiles.push({
-                    name: file.name.split(".")[0] + ".nbt",
+                    name: file.name.split(".")[0] + `.${outputFormat.extension}`,
                     bytes: bd
                 })
             }
         }
 
-        // snbt <-> nbt
-        if (inputFormat.internal == "snbt" && outputFormat.internal == "nbt") {
+        // snbt -> nbt / schem / schematic / litematic
+        if (inputFormat.internal == "snbt" && (outputFormat.internal == "nbt" || outputFormat.internal == "schem" || outputFormat.internal == "schematic" || outputFormat.internal == "litematic")) {
             for (const file of inputFiles) {
                 const text = decoder.decode(file.bytes)
                 const nbt = NBT.parse(text)
-                const bd = await NBT.write(nbt)
+                let bd = await NBT.write(nbt)
+                
+                if (outputFormat.internal == "schem" || outputFormat.internal == "schematic" || outputFormat.internal == "litematic") {
+                    // Schematics require gzipping
+                     bd = gzipSync(bd);
+                }
+
                 outputFiles.push({
-                    name: file.name.split(".")[0] + ".nbt",
+                    name: file.name.split(".")[0] + `.${outputFormat.extension}`,
                     bytes: bd
                 })
             }
         }
-        if (inputFormat.internal == "nbt" && outputFormat.internal == "snbt") {
+        if ((inputFormat.internal == "nbt" || inputFormat.internal == "schem" || inputFormat.internal == "schematic" || inputFormat.internal == "litematic") && outputFormat.internal == "snbt") {
             for (const file of inputFiles) {
-                const nbt = await NBT.read(file.bytes)
+                let unzipped = file.bytes;
+                try {
+                    if (inputFormat.internal == "schem" || inputFormat.internal == "schematic" || inputFormat.internal == "litematic") {
+                        unzipped = gunzipSync(file.bytes);
+                    }
+                } catch (e) {}
+
+                const nbt = await NBT.read(unzipped)
                 const text = NBT.stringify(nbt, {
                     space: this.indent
                 })
@@ -134,7 +165,9 @@ class nbtHandler implements FormatHandler {
             for (const file of inputFiles) {
                 const snbt = decoder.decode(file.bytes)
                 const nbt = NBT.parse(snbt)
-                const text = JSON.stringify(nbt, null, this.indent)
+                const text = JSON.stringify(nbt, (key, value) =>
+                    typeof value === 'bigint' ? value.toString() : value,
+                this.indent)
                 outputFiles.push({
                     name: file.name.split(".")[0] + ".json",
                     bytes: encoder.encode(text)
@@ -151,6 +184,17 @@ class nbtHandler implements FormatHandler {
                 outputFiles.push({
                     name: file.name.split(".")[0] + ".snbt",
                     bytes: encoder.encode(snbt)
+                })
+            }
+        }
+
+        
+        // nbt -> schem / schematic / litematic
+        if (inputFormat.internal == "nbt" && (outputFormat.internal == "schem" || outputFormat.internal == "schematic" || outputFormat.internal == "litematic")) {
+            for (const file of inputFiles) {
+                outputFiles.push({
+                    name: file.name.split(".")[0] + `.${outputFormat.extension}`,
+                    bytes: gzipSync(file.bytes)
                 })
             }
         }
